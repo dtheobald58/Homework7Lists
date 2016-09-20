19bcde5de8e7eea7bb9654aa2caa583bcc4013b5
dtheobald58 <dtheobald58@yahoo.com>
2016-09-19 21:36:56 -0400

    initial commit

---
+++ Homework7Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<bool> boolList = new List<bool>() { true, false, false, true, false };
+            foreach (bool word in boolList)
+            {
+                if (word == true)
+                {
+                    Console.WriteLine("Better bring an umbrella.");
+                }
+                else
+                {
+                    Console.WriteLine("No rain today, enjoy the sun!");
+                }
+
+            }
+        }
+    }
+}
